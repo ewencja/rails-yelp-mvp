7e940b6572b1da24ff7e054ebfa133c9e700c585
Ewa Treitz <ewa.grzechnik@gmail.com>
2016-11-23 17:14:06 +0100

    ex 23.11

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.destroy_all
+restaurants_attributes = [
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    category:     "belgian",
+    phone_number: "01 43 54 23 31"
+  },
+  {
+    name:         "La truffière",
+    address:      "4 rue Blainville 75005 Paris",
+    category:     "french",
+    phone_number:        "01 43 54 23 31"
+  },
+  {
+    name:         "Le pré catelan",
+    address:      "route de Suresnes 75016 Paris",
+    category:     "italian",
+    phone_number: "02 43 54 23 31"
+  }
+]
+restaurants_attributes.each { |params| Restaurant.create!(params) }
